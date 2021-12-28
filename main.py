8e3a3fe21d6ac42f3a765079fde01d72280e9339
Samuel Panek <samuelrpanek@gmail.com>
2021-12-28 12:10:04 -0800

    adds runtime and requirements

---
+++ main.py
@@ -0,0 +1,46 @@
+from typing import List
+
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: str):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+
+            # parse the incoming string to make any sense
+            await manager.broadcast(f"{client_id} says: {data}")
+
+    except WebSocketDisconnect:
+
+        manager.disconnect(websocket)
+
+        await manager.broadcast(f"Client #{client_id} left the chat")
+
